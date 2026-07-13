1388f0559e0c586d7c67dbea8eabd6884b54f77f
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-13 11:49:26 +0530

    Add: reverse-linked-list-ii - submission-5

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-5.js
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} left
+     * @param {number} right
+     * @return {ListNode}
+     */
+    reverseBetween(head, left, right) {
+        let dummy = new ListNode(-1)
+        dummy.next = head
+        let p1 = dummy
+        let pos = 1
+        while (pos < left) {
+            p1 = p1.next
+            pos++
+        }
+        let p2 = p1.next
+        
+
+        let p3 = dummy
+        pos = 1
+        while (pos <= right) {
+            p3 = p3.next
+            pos++
+        }
+        let p4 = p3.next
+        p3.next = null
+
+        p1.next = this.reverseLL(p2)
+        p2.next = p4
+
+        return dummy.next
+    }
+
+    reverseLL(head) {
+        let prev = null
+        let curr = head
+        while (curr !== null) {
+            let nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        }
+        return prev
+    }
+}
