9f0a68fa30f8f947e4522ba465511135b3615a66
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-15 18:37:54 +0530

    Add: design-circular-queue - submission-3

---
+++ Data Structures & Algorithms/design-circular-queue/submission-3.js
@@ -0,0 +1,83 @@
+class MyCircularQueue {
+    /**
+     * @param {number} k
+     */
+    constructor(k) {
+        this.capacity = k
+        this.arr = new Array(k)
+        this.front = 0
+        this.rear = -1
+        this.size = 0
+    }
+
+    /**
+     * @param {number} value
+     * @return {boolean}
+     */
+    enQueue(value) {
+        if (this.isFull()) {
+            return false
+        }
+        this.rear = (this.rear + 1) % this.capacity
+        this.arr[this.rear] = value
+        this.size++
+        return true
+    }
+
+    /**
+     * @return {boolean}
+     */
+    deQueue() {
+        if (this.isEmpty()) {
+            return false
+        }
+        this.front = (this.front + 1) % this.capacity
+        this.size--
+        return true
+    }
+
+    /**
+     * @return {number}
+     */
+    Front() {
+        if (this.isEmpty()) {
+            return -1
+        }
+        return this.arr[this.front]
+    }
+
+    /**
+     * @return {number}
+     */
+    Rear() {
+        if (this.isEmpty()) {
+            return -1
+        }
+        return this.arr[this.rear]
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isEmpty() {
+        return this.size == 0
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isFull() {
+        return this.size == this.capacity
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
