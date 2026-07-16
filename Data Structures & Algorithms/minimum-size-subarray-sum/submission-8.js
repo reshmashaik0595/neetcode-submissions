70384c4c9210805c15333d46d2e4d68c1f2eb499
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-16 22:42:37 +0530

    Add: minimum-size-subarray-sum - submission-8

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-8.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} nums
+     * @return {number}
+     */
+    minSubArrayLen(target, nums) {
+        let pf = 0
+        let l = 0
+        let r = 0
+        let min = +Infinity
+        while (r < nums.length) {
+            pf = pf + nums[r]
+            while (pf >= target) {
+                // min = Math.min(min, r - l + 1)
+                pf = pf - nums[l]
+                l++
+            }
+            console.log(min, l, r, r - (l - 1) + 1)
+            if (l > 0) {
+                min = Math.min(min, r - (l - 1) + 1);
+            }
+            r++
+        }
+        return min == Infinity ? 0 : min
+    }
+}
