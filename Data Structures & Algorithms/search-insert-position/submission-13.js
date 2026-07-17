e3f7b27a48c7489d7282f3e748e236923f050fd4
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-17 19:32:39 +0530

    Add: search-insert-position - submission-13

---
+++ Data Structures & Algorithms/search-insert-position/submission-13.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let s = 0
+        let e = nums.length - 1
+        while (s <= e) {
+            let mid = s + Math.floor((e - s) / 2)
+            if (nums[mid] == target) {
+                return mid
+            }
+
+            if (nums[mid] < target) {
+                s = mid + 1
+            } else {
+                e = mid - 1
+            }
+        }
+        return s
+    }
+}
