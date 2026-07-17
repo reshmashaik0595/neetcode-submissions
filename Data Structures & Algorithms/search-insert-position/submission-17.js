745bacb14521ccf8a8c2f9696e2f3406855848c5
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-17 19:54:09 +0530

    Add: search-insert-position - submission-17

---
+++ Data Structures & Algorithms/search-insert-position/submission-17.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        if (target < nums[0]) {
+            return 0
+        }
+        else if (target > nums[nums.length - 1]) {
+            return nums.length
+        }
+
+        let l = 0
+        let h = nums.length - 1
+        let pos;
+        while (l <= h) {
+            let mid = l + Math.floor((h - l) / 2)
+            if (nums[mid] == target) {
+                return mid
+            }
+
+            if (nums[mid] < target) {
+                pos = mid + 1
+                l = mid + 1
+            } else {
+                pos = mid
+                h = mid - 1
+            }
+        }
+        return pos
+    }
+}
