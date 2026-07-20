77039eb930ffa6329b91dc85d486e252baf06b0e
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-20 21:12:36 +0530

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    inorderTraversal(root) {
+        if (root == null) return []
+        this.ans = []
+        this.solve(root)
+        return this.ans
+    }
+
+    solve(root) {
+        if (root == null) {
+            return
+        }
+        this.solve(root.left)
+        this.ans.push(root.val)
+        this.solve(root.right)
+    }
+}
