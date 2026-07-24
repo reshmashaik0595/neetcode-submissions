1adb7bc5e232e17c63cb0fd9b344d883039c8e30
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-07-24 22:27:11 +0530

    Add: balanced-binary-tree - submission-5

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-5.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        function height(root) {
+            if (root == null) {
+                return 0
+            }
+            let left = height(root.left)
+            if (left == -1) return -1
+            let right = height(root.right)
+            if (right == -1) return -1
+            if (Math.abs(left - right) > 1) {
+                return -1
+            }
+            return 1 + Math.max(left, right)
+        }
+
+        return height(root) !== -1
+    }
+}
