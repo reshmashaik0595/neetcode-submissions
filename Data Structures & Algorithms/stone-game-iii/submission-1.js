31be9d8c860b91430ee287c74bf44efd47af5434
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-09 14:50:44 +0530

    Add: stone-game-iii - submission-1

---
+++ Data Structures & Algorithms/stone-game-iii/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} stoneValue
+     * @return {string}
+     */
+    stoneGameIII(stoneValue) {
+        let max = -Infinity
+        function solve(idx) {
+            if (idx >= stoneValue.length) return 0
+            if (dp[idx] !== -1) return dp[idx]
+            max = Math.max(max, stoneValue[idx] - solve(idx + 1))
+            if (idx + 1 < stoneValue.length) max = Math.max(max, stoneValue[idx] + stoneValue[idx + 1] - solve(idx + 2))
+            if (idx + 2 < stoneValue.length) max = Math.max(max, stoneValue[idx] + stoneValue[idx + 1] + stoneValue[idx + 2] - solve(idx + 3))
+            return dp[idx] = max
+        }
+        
+        let dp = new Array(stoneValue.length + 1).fill(-1)
+        let ans = solve(0)
+        return ans == 0 ? 'Tie' : (ans > 0) ? 'Alice' : 'Bob'
+
+    }
+}
