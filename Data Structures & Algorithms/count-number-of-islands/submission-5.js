2ffb2c6fc81be6ed4f9f2f7edc93c3f60a86401c
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-17 14:50:58 +0530

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        function dfs(r, c) {
+            if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length) return
+            if (isVisited[r][c]) return
+            if (grid[r][c] == '0') return
+            isVisited[r][c] = true
+            let neigh = [
+                [-1, 0],
+                [0, -1],
+                [1, 0],
+                [0, 1]
+            ]
+            for (let i = 0; i < neigh.length; i++) {
+                let nRow = r + neigh[i][0]
+                let nCol = c + neigh[i][1]
+                dfs(nRow, nCol)
+            }
+        }
+
+        let count = 0
+        let isVisited = new Array(grid.length)
+        for (let i = 0; i < isVisited.length; i++) {
+            isVisited[i] = new Array(grid[0].length).fill(false)
+        }
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                if (isVisited[i][j]) continue
+                if (grid[i][j] == '0') continue
+                count++
+                dfs(i, j)
+            }
+        }
+        return count
+    }
+}
