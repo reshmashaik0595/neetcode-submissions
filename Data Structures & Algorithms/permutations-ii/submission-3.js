af9e61251206a6bd10b4db53efdc5542c1862148
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-18 20:30:13 +0530

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permuteUnique(nums) {
+        nums = nums.sort((a, b) => a - b)
+        function solve(idx, curr) {
+            if (curr.length == nums.length) {
+                result.push([...curr])
+                return
+            }
+            for (let i = 0; i < nums.length; i++) {
+                if (i > 0 && nums[i] == nums[i - 1] && !isUsed[i - 1]) continue
+                if (isUsed[i]) continue
+                isUsed[i] = true
+                curr.push(nums[i])
+                solve(idx + 1, curr)
+                isUsed[i] = false
+                curr.pop()
+            }
+        }
+        let result = []
+        let isUsed = new Array(nums.length).fill(false)
+        solve(0, [])
+        return result
+    }
+}
