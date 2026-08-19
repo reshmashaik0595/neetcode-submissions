e8475290503d007a63be4e526f9a9832d462a154
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-19 22:23:44 +0530

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        function solve(nums, temp, isUsed) {
+            if (temp.length == nums.length) {
+                result.push([...temp])
+                return
+            }
+            for (let i = 0; i < nums.length; i++) {
+                if (isUsed[i]) continue
+                isUsed[i] = true
+                temp.push(nums[i])
+                solve(nums, temp, isUsed)
+                isUsed[i] = false
+                temp.pop()
+            }
+        }
+
+        let result = []
+        let temp = []
+        let isUsed = new Array(nums.length).fill(false)
+        solve(nums, temp, isUsed)
+        return result
+    }
+}
