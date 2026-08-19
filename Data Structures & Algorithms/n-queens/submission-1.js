780dd6c1be1534cb67f9f326be84a642b568a779
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-19 16:29:34 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[][]}
+     */
+    solveNQueens(n) {
+        function isValid(board, row, col) {
+            for (let r = row - 1; r >= 0; r--) {
+                if (board[r][col] == 'Q') return false
+            }
+
+            for (let r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
+                if (board[r][c] == 'Q') return false
+            }
+
+            for (let r = row - 1, c = col + 1; r >= 0 && c < board.length; r--, c++) {
+                if (board[r][c] == 'Q') return false
+            }
+
+            return true
+        }
+
+        function solve(board, row) {
+            if (row >= board.length) {
+                ans.push(board.map(ele => ele.join('')))
+                return
+            }
+            for (let col = 0; col < board[0].length; col++) {
+                if (isValid(board, row, col)) {
+                    board[row][col] = 'Q'
+                    solve(board, row + 1)
+                    board[row][col] = '.'
+                }
+            }
+        }
+
+        let board = new Array(n)
+        for (let i = 0; i < board.length; i++) {
+            board[i] = new Array(n).fill('.')
+        }
+
+        let ans = []
+        solve(board, 0)
+        return ans
+    }
+}
