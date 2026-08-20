1864ee44ef7177ec77a3025b99f795aa713e63f1
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-20 12:10:24 +0530

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {string[]}
+     */
+    wordBreak(s, wordDict) {
+        function solve(idx, currSentence) {
+            if (idx >= s.length) {
+                result.push(currSentence)
+                return
+            }
+
+            for (let i = idx; i < s.length; i++) {
+                let str = s.substring(idx, i + 1)
+                let nextSentence = currSentence
+                if (words.has(str)) {
+                    if (idx > 0) {
+                        nextSentence = nextSentence + " "
+                    }
+                    solve(i + 1, nextSentence + str)
+                }
+            }
+        }
+
+        let result = []
+        let currSentence = ""
+        let words = new Set([...wordDict])
+        let idx = 0
+        solve(idx, currSentence)
+        return result
+    }
+}
