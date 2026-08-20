c65dedaf301219a5753420d2fb2c94bd8c08e3c6
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-20 12:56:59 +0530

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let currSum = 0
+        let maxSum = -Infinity
+        for (let i = 0; i < nums.length; i++) {
+            if (currSum <= 0) {
+                currSum = 0
+            }
+            currSum = currSum + nums[i]
+            maxSum = Math.max(maxSum, currSum)
+            
+        }
+        return maxSum
+    }
+}
