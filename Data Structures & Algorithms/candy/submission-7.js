7a777b2c096e7ced0201eecedd3b18364206e25f
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-20 19:53:35 +0530

    Add: candy - submission-7

---
+++ Data Structures & Algorithms/candy/submission-7.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} ratings
+     * @return {number}
+     */
+    candy(ratings) {
+        let ltr = new Array(ratings.length).fill(1)
+        let rtl = new Array(ratings.length).fill(1)
+        // left -> right
+        for (let i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                ltr[i] = ltr[i - 1] + 1
+            }
+        }
+        // right -> left
+        for (let i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                rtl[i] = rtl[i + 1] + 1
+            }
+        }
+
+        let ans = 0
+        for (let i = 0; i < ratings.length; i++) {
+            ans = ans + Math.max(ltr[i], rtl[i])
+        }
+        return ans
+    }
+}
+
+
