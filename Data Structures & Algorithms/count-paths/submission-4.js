7bee511f09cdd07062aac07314d910e8e6e5099c
reshmashaik0595 <53150487+reshmashaik0595@users.noreply.github.com>
2026-08-25 23:25:30 +0530

    Add: count-paths - submission-4

---
+++ Data Structures & Algorithms/count-paths/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        function solve(row, col) {
+            if (row >= m || col >= n) return 0
+            if (row == m - 1 && col == n - 1) return 1
+
+            if (dp[row][col] !== -1) return dp[row][col]
+            return dp[row][col] = solve(row + 1, col) + solve(row, col + 1)
+        }
+        let dp = new Array(m)
+        for (let i = 0; i < m; i++) {
+            dp[i] = new Array(n + 1).fill(-1)
+        }
+        return solve(0, 0)
+    }
+}
